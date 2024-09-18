6f11e14db0b14942ad0d78a40c62a1bac6dd4e58
Jeongminseok-web <jeong010328@gmail.com>
2024-09-18 14:22:48 +0900

    oauth

--- front/src/index.js
+++ front/src/index.js
@@ -9,10 +9,10 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </GoogleOAuthProvider>
+    {/* <GoogleOAuthProvider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+    {/* </GoogleOAuthProvider> */}
   </React.StrictMode>
 );
